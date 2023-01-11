af1ef2cef6ef1b6a6b8251771d58c10c2b225e7a
angrycoderz <angrycoderz01@gmail.com>
2023-01-11 09:35:35 +0500

    yml

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import nextConfig from '../next.config'
 
 const Home = () => {
+    console.log(nextConfig.APP_NAME)
     return (
         <div className='d-flex justify-content-center align-items-center'
             style={{ width: "100%", height: "100vh" }}
         >
             <Link href="/shop" id="link">
                 Shop Page
-                {process.env.APP_NAME}
+                
+                {nextConfig.NAME}
             </Link>
         </div>
     )
