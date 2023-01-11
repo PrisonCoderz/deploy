cb046cdf0900480718b32e78f8932b7008418d8b
angrycoderz <angrycoderz01@gmail.com>
2023-01-11 11:13:10 +0500

    fix env

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
+import nextConfig from '../next.config' 
 
 const Home = () => {
-    console.log(process.env.APP_NAME)
+    console.log(nextConfig.APP_NAME)
     return (
         <div className='d-flex justify-content-center align-items-center'
             style={{ width: "100%", height: "100vh" }}
