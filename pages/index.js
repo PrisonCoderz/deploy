4eb00d12b85297a5caf8febe38e8e3467ca10439
angrycoderz <angrycoderz01@gmail.com>
2023-01-12 08:42:10 +0500

    nextConfig10

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,9 @@ import Link from 'next/link'
 // import { PASSWORD } from '../config'
 import getConfig from 'next/config'
 const Home = () => {
-    const { publicRuntimeConfig } = getConfig()
+    // const { publicRuntimeConfig } = getConfig()
     console.log(process.env.NEXT_PUBLIC_APP_NAME)
     // console.log(PASSWORD)
-    console.log(publicRuntimeConfig)
     return (
         <div className='d-flex justify-content-center align-items-center'
             style={{ width: "100%", height: "100vh" }}
